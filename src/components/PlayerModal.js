b1bc54a2a5a38ee6df3f0865055f889996061557
Nic Raboy <nic@nraboy.com>
2022-06-08 10:50:58 -0700

    Clean Up Modal
    Changed styles and colors. Added images. Fixed the index to use the correct data.

--- src/components/PlayerModal.js
+++ src/components/PlayerModal.js
@@ -10,39 +10,51 @@ const PlayerModal = ({
 }) => {
   return (
     <div className="fixed inset-0 z-20 p-20 flex justify-center bg-slate-700 ">
-      <div className="relative flex flex-col w-2/3 bg-white  border border-black rounded h-2/3 mt-60 ">
-        <div className="p-4 mb-4 text-4xl font-bold text-center text-tolopea-500 "></div>
-        <div className="flex justify-around py-4 pl-2 pr-4">
-          {/* <img
-            src={player.player_face_url}
+      <div className="relative flex flex-col w-2/3 bg-gray-900 text-white border-2 border-white rounded pt-5">
+        <div className="flex py-4 pl-2 pr-4">
+        <div className="w-1/2">
+          <img
+            src={players[playerIndex].player_face_url}
             alt="player"
-            className="object-contain w-1/2 my-auto rounded-full"
-          /> */}
-          <div className="flex flex-col ml-6 text-2xl ">
-            <div>Working HERE</div>
-            <div> Name: {players[0].long_name}</div>
-            <div> Club Jersey Number: {players[0].club_jersey_number}</div>
-            <div> Club: {players[0].club_name}</div>
+            className="object-contain w-full my-auto rounded"
+          />
+          <div className="flex flex-row justify-center p-2">
+            <img
+                src={players[playerIndex].nation_flag_url}
+                alt="flag"
+                className="object-contain w-12 my-auto rounded mr-2"
+            />
+            <img
+                src={players[playerIndex].club_logo_url}
+                alt="club"
+                className="object-contain w-12 my-auto rounded-full"
+            />
+          </div>
+          </div>
+          <div className="flex flex-col ml-6 text-2xl flex-grow">
+            <div> Name: {players[playerIndex].long_name}</div>
+            <div> Club Jersey Number: {players[playerIndex].club_jersey_number}</div>
+            <div> Club: {players[playerIndex].club_name}</div>
             <div>
               {" "}
-              National Jersey Number: {players[0].nation_jersey_number}
+              National Jersey Number: {players[playerIndex].nation_jersey_number}
             </div>
-            <div> Nationality: {players[0].nationality_name}</div>
-            <div> Overall{players[0].overall}</div>
-            <div> Pace: {players[0].pace}</div>
-            <div> Dribbling: {players[0].dribbling}</div>
-            <div> Shooting: {players[0].shooting}</div>
-            <div> Defending: {players[0].defending}</div>
-            <div> Physical: {players[0].physic}</div>
-            <div> Skill Moves: {players[0].skill_moves}</div>
-            <div> Weak Foot:{players[0].weak_foot}</div>
-            <div>DOB: {players[0].dob}</div>
-            <div>WAGE_EUR: {players[0].wage_eur}</div>
-            <div>AGE: {players[0].age}</div>
+            <div> Nationality: {players[playerIndex].nationality_name}</div>
+            <div> Overall{players[playerIndex].overall}</div>
+            <div> Pace: {players[playerIndex].pace}</div>
+            <div> Dribbling: {players[playerIndex].dribbling}</div>
+            <div> Shooting: {players[playerIndex].shooting}</div>
+            <div> Defending: {players[playerIndex].defending}</div>
+            <div> Physical: {players[playerIndex].physic}</div>
+            <div> Skill Moves: {players[playerIndex].skill_moves}</div>
+            <div> Weak Foot:{players[playerIndex].weak_foot}</div>
+            <div>DOB: {players[playerIndex].dob}</div>
+            <div>WAGE_EUR: {players[playerIndex].wage_eur}</div>
+            <div>AGE: {players[playerIndex].age}</div>
           </div>
         </div>
         <div
-          className="absolute bottom-0 left-0"
+          className="absolute bottom-0 left-0 ml-3 mb-3"
           onClick={() => {
             setShowPlayerModal(false);
             setPlayerIndex(-100);
@@ -63,7 +75,7 @@ const PlayerModal = ({
           </svg>
         </div>
         <div
-          className="absolute bottom-0 right-0"
+          className="absolute bottom-0 right-0 mr-3 mb-3"
           onClick={() => {
             console.log("CLICK ADD PLAYER: ", players[playerIndex].short_name);
             addPlayerToTeam(players[playerIndex], position2Fill);
